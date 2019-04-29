9ad0d1f671c4baa687e70829228d42fc4d37216b
Bohyeon Kim <bh.kim@nemolab.co.kr>
2019-04-29 18:49:50 +0900

    images changed

--- vue.js
+++ vue.js
@@ -56,102 +56,122 @@ let fivthApp = new Vue({ //v-on
             {
                 serviceName:'문서 출력',
                 whatPage:1,
-                serviceDetail: '문서출력은 이런저런 것입니다.',
+                serviceDetail: '비즈니스에 필요한 문서를 출력하여 제공합니다.',
+                imageNum: 'printImage01.jpg'
             }, 
+            {
+                serviceName:'컬러 출력',
+                whatPage:1,
+                serviceDetail: '1200dpi의 고화질 출력으로 최상의 출력품질을 보여드립니다..',
+                imageNum: 'printImage02.jpg'
+            },  
+            {
+                serviceName:'대형 출력',
+                whatPage:1,
+                serviceDetail: '단 1장이라도 대형 출력으로 빠르게 소량 포스터를 제작해보세요.',
+                imageNum: 'printImage03.jpg'
+            },   
             {
                 serviceName:'고급 제본',
                 whatPage:1,
-                serviceDetail: '문서출력은 이런저런 것입니다.',
-            },  
-            {
-                serviceName:'문서 출력3',
-                whatPage:1,
-                serviceDetail: '문서출력은 이런저런 것입니다.',
+                serviceDetail: '무선, 트윈링(스프링), 중철, PUR 등 기본적인 제본 뿐만 아니라, 실제본, 양장제본 고서제본 등 다양한 제본방법을 적소에 제공합니다.',
+                imageNum: 'printImage04.jpg'
             },   
             {
-                serviceName:'문서 출력4',
-                whatPage:1,
-                serviceDetail: '문서출력은 이런저런 것입니다.',
-            },   
-            {
-                serviceName:'문서 출력5',
+                serviceName:'현수막, 배너',
                 whatPage:2,
-                serviceDetail: '문서출력은 이런저런 것입니다.',
+                serviceDetail: '종이뿐만 아니라 현수막, 배너, 포토월 등 다양한 대형 실사이즈 출력물을 빠르게 제공합니다.',
+                imageNum: 'printImage05.jpg'
             },  
             {
                 serviceName:'문서 출력6',
                 whatPage:2,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage06.jpg'
             },  
             {
                 serviceName:'문서 출력7',
                 whatPage:2,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage07.jpg'
             },  
             {
                 serviceName:'문서 출력8',
                 whatPage:2,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage08.jpg'
             },
             {
                 serviceName:'문서 출력9',
                 whatPage:3,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage09.jpg'
             }, 
             {
                 serviceName:'문서 출력10',
                 whatPage:3,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage10.jpg'
             },  
             {
                 serviceName:'문서 출력11',
                 whatPage:3,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage11.jpg'
             },   
             {
                 serviceName:'문서 출력12',
                 whatPage:3,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage12.jpg'
             },   
             {
                 serviceName:'문서 출력13',
                 whatPage:4,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage13.jpg'
             },  
             {
                 serviceName:'문서 출력14',
                 whatPage:4,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage14.jpg'
             },  
             {
                 serviceName:'문서 출력15',
                 whatPage:4,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage15.jpg'
             },  
             {
                 serviceName:'문서 출력16',
                 whatPage:4,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage16.jpg'
             },
             {
                 serviceName:'문서 출력17',
                 whatPage:5,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage17.jpg'
             }, 
             {
                 serviceName:'문서 출력18',
                 whatPage:5,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage18.jpg'
             },  
             {
                 serviceName:'문서 출력19',
                 whatPage:5,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage19.jpg'
             },   
             {
                 serviceName:'문서 출력20',
                 whatPage:5,
                 serviceDetail: '문서출력은 이런저런 것입니다.',
+                imageNum: 'printImage20.jpg'
             }
         ]   
     },
